1a9c9618bca23b4bead74eaa58dad7286ee30482
bonnie <owu.owu.666@gmail.com>
2025-05-13 11:26:40 -0400

    front end basic setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import NavBar from "./components/NavBar/NavBar.jsx";
+// import SignUpForm from "./components/SignUpForm/SignUpForm.jsx";
+// import SignInForm from "./components/SignInForm/SignInForm.jsx";
+// import Landing from "./components/Landing/Landing.jsx";
+// import Dashboard from "./components/Dashboard/Dashboard.jsx";
+// import Show from "./components/Show/Show.jsx";
+// import { useContext } from "react";
+import { UserContext } from "./contexts/UserContext.jsx"
+import { Routes, Route } from "react-router";
+import "./App.css";
+
+function App() {
+  // const { user } = useContext(UserContext);
+
+  return (
+    <>
+      {/* <NavBar /> */}
+      <Routes>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
