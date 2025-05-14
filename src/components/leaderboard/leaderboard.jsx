d90bf2698742a80f8d33770ad9a8ebad536bec43
StellaTeuscher <stella.teuscher@gmail.com>
2025-05-14 14:03:22 -0400

    got the leaderboard to work just need to fix cohort

--- src/components/leaderboard/leaderboard.jsx
+++ src/components/leaderboard/leaderboard.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState, useNavigate } from "react";
+import { Link } from "react-router"
+import { getUsers } from '../../services/userService';
 
 function Leaderboard() {
+  // const navigate = useNavigate();
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async() => {
+      const usersData = await getUsers()
+      setUsers(usersData)
+    }
+
+    fetchUsers()
+  }, [])
+
   return (
-    <div>Leaderboard</div>
+    <div className='leaderboard-page'>
+      <h1>Leaderboard</h1>
+      <div className="cohort-dropdown">
+        <button className='Cohort'></button>
+      </div>
+      <div className="leaderboard-container">
+        <ol>
+        {users.length && users.map((user) => (
+          <div key={user.id} className="user-card">
+            <li>
+              <Link to={`/profiles/${user.id}`}>
+              </Link>
+              <h2>{user.user.username}</h2>
+              <p>{user.total_points}</p>
+              </li>
+            </div>
+        ))}
+        </ol>
+      </div>
+    </div>
   )
 }
 
